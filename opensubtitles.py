ac01ec79d19040553d34b622f9cd4f0991dfacf8
Satish Mishra <zicsxone@gmail.com>
2023-08-13 17:52:38 +0530

    decomposed download_subtitles

--- opensubtitles.py
+++ opensubtitles.py
@@ -10,16 +10,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from traceback import format_exc
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
 
-# Global requests session
+# Constants
+SUBTITLES_DIR = "subtitles"
+BASE_URL = "https://www.opensubtitles.org/en/search/sublanguageid-hin/offset-"
 SESSION = requests.Session()
+
 
 def setup_driver():
     options = Options()
-    
     # Common settings
     options.add_argument("--disable-extensions")
     options.add_argument("--disable-gpu")
@@ -31,10 +34,11 @@ def setup_driver():
     if platform == "win32":
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
     elif platform.startswith("linux"):
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
     return webdriver.Chrome(options=options)
+
 
 def download_and_extract_zip(download_link):
     zip_response = SESSION.get(download_link)
@@ -42,12 +46,28 @@ def download_and_extract_zip(download_link):
 
     srt_files = [name for name in z.namelist() if name.endswith(".srt")]
     for srt_file in srt_files:
-        z.extract(srt_file, path="subtitles")
+        z.extract(srt_file, path=SUBTITLES_DIR)
 
     return srt_files
 
+
+def get_title_and_year_from_filename(filename):
+    year_match = re.search(r"\b\d{4}\b", filename)
+    year = year_match.group(0) if year_match else "N/A"
+
+    title = re.sub(
+        r"\b\d{4}\b|S\d{2}E\d{2}|\d+p|WEB|HDTV|x264|x265|MiNX|HIN|[-_.]",
+        " ",
+        filename,
+        flags=re.I,
+    )
+    title = re.sub(r"\s+", " ", title).strip()
+
+    return title, year
+
+
 def download_subtitles(driver, writer, file):
-    driver.get("https://www.opensubtitles.org/en/search/sublanguageid-hin/offset-0")
+    driver.get(BASE_URL + "0")
     total_entries_element = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located(
             (By.CSS_SELECTOR, "div.msg.hint span b:nth-child(3)")
@@ -57,12 +77,13 @@ def download_subtitles(driver, writer, file):
 
     for offset in range(0, total_entries, 40):
         try:
-            driver.get(
-                f"https://www.opensubtitles.org/en/search/sublanguageid-hin/offset-{offset}"
-            )
+            driver.get(BASE_URL + str(offset))
             WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located(
-                    (By.CSS_SELECTOR, "tr.change.even.expandable, tr.change.odd.expandable")
+                    (
+                        By.CSS_SELECTOR,
+                        "tr.change.even.expandable, tr.change.odd.expandable",
+                    )
                 )
             )
             entries = driver.find_elements(
@@ -71,33 +92,28 @@ def download_subtitles(driver, writer, file):
 
             for entry in entries:
                 download_id = entry.get_attribute("id").replace("name", "")
-                download_link = f"https://www.opensubtitles.org/en/subtitleserve/sub/{download_id}"
+                download_link = (
+                    f"https://www.opensubtitles.org/en/subtitleserve/sub/{download_id}"
+                )
 
                 srt_files = download_and_extract_zip(download_link)
 
                 for srt_file in srt_files:
-                    year_match = re.search(r"\b\d{4}\b", srt_file)
-                    year = year_match.group(0) if year_match else "N/A"
-
-                    title = re.sub(
-                        r"\b\d{4}\b|S\d{2}E\d{2}|\d+p|WEB|HDTV|x264|x265|MiNX|HIN|[-_.]",
-                        " ",
-                        srt_file,
-                        flags=re.I,
-                    )
-                    title = re.sub(r"\s+", " ", title).strip()
-
+                    title, year = get_title_and_year_from_filename(srt_file)
                     writer.writerow([title, year, srt_file])
                     file.flush()
 
         except Exception as e:
-            logging.error(f"Error processing an entry: {e}")
+            logging.error(f"Error processing an entry: {e}\n{format_exc()}")
 
 
 def main():
     driver = setup_driver()
-    if not os.path.exists("subtitles"):
-        os.makedirs("subtitles")
+
+    # Ensure the subtitles directory exists
+    if not os.path.exists(SUBTITLES_DIR):
+        os.makedirs(SUBTITLES_DIR)
+
     try:
         with open("subtitles.csv", "w", newline="", encoding="utf-8") as file:
             writer = csv.writer(file)
@@ -107,10 +123,11 @@ def main():
         logging.info("Script completed successfully.")
 
     except Exception as e:
-        logging.error(f"Error: {e}")
+        logging.error(f"Error: {e}\n{format_exc()}")
 
     finally:
         driver.quit()
+
 
 if __name__ == "__main__":
     main()
